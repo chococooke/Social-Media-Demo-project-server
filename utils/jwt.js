fc2dac69622e19b753c9046d26a0d38b1912beac
chococooke * web3 <pro.bhimsen@gmail.com>
2024-02-22 22:02:24 +0530

    remove reduntant code

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,43 +1,39 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const secret = process.env.JWT_SECRET;
+
 const getTokenFromHeaders = (authHeader) => {
-    if (!authHeader) {
-        throw new Error("Authorization headers not present");
-    }
+    if (!authHeader) throw new Error("Authorization headers not present");
 
     const parts = authHeader.split(' ');
-    if (parts.length !== 2 || parts[0] !== 'Bearer') {
-        throw new Error("Token is malformed");
-    }
+
+    if (parts.length !== 2 || parts[0] !== 'Bearer') throw new Error("Token is malformed");
 
     return parts[1];
 }
 
 const sign = (payload) => new Promise((res, rej) => {
-    if (payload) {
-        jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
-            if (err) {
-                rej(err);
-            } else {
-                res(token)
-            }
-        });
-    } else {
-        throw new Error("Token is not present");
+    if (!payload) throw new Error("No data provided");
+
+    try {
+        const token = jwt.sign(payload, secret);
+        res(token)
+    } catch (error) {
+        rej("No valid data")
     }
-});
+})
 
 const verify = (payload) => new Promise((res, rej) => {
     const token = getTokenFromHeaders(payload);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            rej(err);
-        } else {
-            res(decoded)
-        }
-    })
+    try {
+        const decodedToken = jwt.verify(token, secret);
+        res(decodedToken);
+    } catch (err) {
+        console.log(err);
+        rej(err);
+    }
 });
 
 module.exports = { verify, sign }
